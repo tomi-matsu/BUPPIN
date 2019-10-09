97b9ee0dd2214f538b3f620e04011f002141207a
h.matsumoto <h.matsumoto@corp.acall.jp>
2019-10-09 13:37:30 +0900

    markup footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
-  v-bottom-navigation.footer(fixed)
-    v-btn(value="recent")
-      span Recent
-      v-icon mdi-history
-    v-btn(value="favorites")
-      span Favorites
-      v-icon mdi-heart
-    v-btn(value="nearby")
-      span Nearby
-      v-icon mdi-map-marker
+  .footer
+    .side-menu-wrap
+      .side-menu.left
+        v-icon.icon mdi-format-list-bulleted
+        span.label list
+      .side-menu.right
+        v-icon.icon mdi-settings-outline
+        span.label setting
+    .home
+
 </template>
 
 <script lang="ts" scoped>
@@ -19,7 +19,35 @@ export default {
   },
 }
 </script>
-<style lang="sass">
-.footer
-  justify-content: space-between
+<style lang="scss">
+.footer {
+  width: 100vw;
+  height: 48px;
+  position: fixed;
+  bottom: 0;
+  background-color: #EAEAEA;
+}
+.side-menu-wrap {
+  display: flex;
+  justify-content: space-between;
+}
+.side-menu {
+  width: 33vw;
+  text-align: center;
+  display: grid;
+  padding: .2em .5em;
+  .label {
+    font-size: .8em;
+  }
+}
+.home {
+  width: 33vw;
+  height: 33vw;
+  background-color: #FACE3B;
+  position: fixed;
+  bottom: calc(-33vw / 2);
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 50%;
+}
 </style>
